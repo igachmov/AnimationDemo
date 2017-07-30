e4302b99e1e8a79f40ee885a555419293df8234e
igachmov <ivan.gachmov@abv.bg>
2017-07-30 13:44:35 +0300

    First commit

---
+++ app/src/main/java/com/example/ivan/animationdemo/AnimationFragment.java
@@ -0,0 +1,21 @@
+package com.example.ivan.animationdemo;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AnimationFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_animation, container, false);
+        return root;
+    }
+
+}
