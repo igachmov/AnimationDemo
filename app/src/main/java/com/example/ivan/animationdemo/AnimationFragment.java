da84c92c8805fa616a99d011848efb570dfbd2f4
igachmov <ivan.gachmov@abv.bg>
2017-07-30 19:46:32 +0300

    Second commit

--- app/src/main/java/com/example/ivan/animationdemo/AnimationFragment.java
+++ app/src/main/java/com/example/ivan/animationdemo/AnimationFragment.java
@@ -2,20 +2,71 @@ package com.example.ivan.animationdemo;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.DialogFragment;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-public class AnimationFragment extends Fragment {
+public class AnimationFragment extends DialogFragment {
+    private GestureDetector gesture;
+
+    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_MAX_OFF_PATH = 250;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_animation, container, false);
+        gesture = new GestureDetector(getActivity(), new MyGestureListener());
+        root.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                gesture.onTouchEvent(event);
+                return true;
+            }
+        });
         return root;
     }
+
+    @Override
+    public void onActivityCreated(Bundle arg0) {
+        super.onActivityCreated(arg0);
+        getDialog().getWindow()
+                .getAttributes().windowAnimations = R.style.DialogAnimation;
+    }
+
+
+
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            try {
+                if (e1.getY() - e2.getY() > SWIPE_MAX_OFF_PATH  && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY){
+                   getDialog().dismiss();
+                    return true;
+                }
+                if (Math.abs(e1.getX() - e2.getX()) > SWIPE_MAX_OFF_PATH) {
+                    return false;
+                }
+
+            } catch (Exception e) {
+
+            }
+            return false;
+        }
+
+    }
+
 
 }
--- app/src/main/java/com/example/ivan/animationdemo/MainActivity.java
+++ app/src/main/java/com/example/ivan/animationdemo/MainActivity.java
@@ -2,16 +2,14 @@ package com.example.ivan.animationdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
     private Button clickMe;
     private AnimationFragment animationFragment;
-    private GestureDetector gesture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,43 +18,15 @@ public class MainActivity extends AppCompatActivity {
 
         clickMe = (Button) findViewById(R.id.button_animation);
         animationFragment = new AnimationFragment();
-        gesture = new GestureDetector(this, new MyGestureListener());
+
 
         clickMe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                clickMe.setVisibility(View.INVISIBLE);
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .setCustomAnimations(
-                                R.anim.slide_in_top,
-                                R.anim.slide_out_bottom)
-                        .replace(R.id.fragment_container,animationFragment)
-                        .commit();
+                animationFragment.show(getSupportFragmentManager(),"fragment");
             }
         });
 
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        return gesture.onTouchEvent(event);
-    }
-
-    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            clickMe.setVisibility(View.VISIBLE);
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .setCustomAnimations(
-                            R.anim.slide_in_bottom,
-                            R.anim.slide_out_top)
-                    .remove(animationFragment)
-                    .commit();
-            return true;
-        }
     }
 
 }
